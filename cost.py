cdd05e93e2597b7fdf1b34e1f937a2dff7afefaa
JacobPackman <jacobvdi@VDI-ITS04.localdomain>
2020-10-12 10:19:53 -0500

    Working Python Script

--- cost.py
+++ cost.py
@@ -1,26 +1,29 @@
 def cust_cost(unit):
     unit = round(unit + unit*.2, 2)
+    return unit
 
-def cost(pc,dock,cables,monitors,accessories):
-    pc = input("Enter the cost of the PC")
+def cost():
+    pc = input("Enter the cost of the PC: ")
     pc_cost = cust_cost(pc)
     
-    dock = input("Enter the cost of the dock")
-    dock = cust_cost(dock)
+    dock = input("Enter the cost of the dock: ")
+    dock_cost = cust_cost(dock)
 
-    cables = input("Enter the cost of the cables")
-    cables = cust_cost(cables)
-    
-    monitors = input("Enter the cost of the monitors")
-    monitors = cust_cost(monitors)
+    cables = input("Enter the cost of the cables: ")
+    cables_cost = cust_cost(cables)
 
-    accessories = input("Enter the cost of all miscellaneous accessories")
-    accessories = cust_cost(accessories)
+    monitors = input("Enter the cost of the monitors: ")
+    monitors_cost = cust_cost(monitors)
 
-    total = pc + dock + cables + monitors + accessories
+    accessories = input("Enter the cost of all miscellaneous accessories: ")
+    accessories_cost = cust_cost(accessories)
 
-cost(pc,dock,cables,monitors,accessories)
+    total = pc_cost + dock_cost + cables_cost + monitors_cost + accessories_cost
+    print(" The cost of the PC is {}.\n The cost of the dock is {}.\n The cost of the cabling is {}.\n The cost of the monitors is {}.\n The cost of all other accessories is {}.\n The total is {}".format(pc_cost, dock_cost, cables_cost, monitors_cost, accessories_cost, total))
 
+cost()
+
+"""
 if pc >= 0:
     print(f"The cost of the pc is {pc}")
 else:
@@ -47,7 +50,7 @@ else:
     pass
 
 print(f"The total is {total} before tax.")
-
+"""
 
 
 
