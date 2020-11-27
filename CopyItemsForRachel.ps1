9499edecada505bf4416bab32c7cc880e4f4573b
JacobPackman <jacob@DESKTOP-9PJPT8H.localdomain>
2020-11-27 08:44:36 -0600

    Added PS to pull all images in directory and copy over to secondary location while preserving file structured

---
+++ CopyItemsForRachel.ps1
@@ -0,0 +1,12 @@
+$folders = gci -recurse | where {$_.Name -match "\w+jpg|\w+gif|\w+tif|\w+jpeg|\w+tiff|\w+bmp|\w+png|\w+raw"}
+
+foreach ($fol in $folders) { 
+    $FolderName = ($fol).Directory.Name
+    $Backup = "$HOME\Backup\"
+    $BackupFolderName = $backup + $FolderName
+    if ( (Test-Path $BackupFolderName) -eq $true)
+        {Copy-Item ($fol).FullName -Destination $BackupFolderName}
+    else 
+        { mkdir $BackupFOlderName; 
+          cp ($fol).FullName -Destination $BackupFolderName }
+}
