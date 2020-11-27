70014d17104456b361b9128ce3c5752b9d4eb8b7
JacobPackman <jacob@DESKTOP-9PJPT8H.localdomain>
2020-11-27 08:39:10 -0600

    Privacy changes

--- CopyItemsForRachel.ps1
+++ CopyItemsForRachel.ps1
@@ -2,7 +2,8 @@ $folders = gci -recurse | where {$_.Name -match "\w+jpg|\w+gif|\w+tif|\w+jpeg|\w
 
 foreach ($fol in $folders) { 
     $FolderName = ($fol).Directory.Name
-    $BackupFolderName = "C:\Users\Jacob\Desktop\RachelBackup\" + $FolderName
+    $Backup = "$HOME\Backup\"
+    $BackupFolderName = $backup + $FolderName
     if ( (Test-Path $BackupFolderName) -eq $true)
         {Copy-Item ($fol).FullName -Destination $BackupFolderName}
     else 
