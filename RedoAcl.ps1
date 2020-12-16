395f7d7096a2c5a4568f3f6d312d725f8a52186f
Jacob Packman <jacob.packman@vdi-inc.com>
2020-12-16 10:56:26 -0600

    Script actually working now

--- RedoAcl.ps1
+++ RedoAcl.ps1
@@ -5,13 +5,14 @@ foreach ($fol in $folders) {
     $acl.SetAccessRuleProtection($true,$true)
     Set-Acl -path $fol -AclObject $acl
     
-    $acl = Get-Acl $fol
-    $rule = $acl.Access | ? {$_.IdentityReference -match ".*txcosc\\[^(s|d)]" -or $_.IdentityReference -Match ".*Domain Users.*"}
-    $acl.RemoveAccessRule($rule)
-    Set-Acl -path $fol -AclObject $acl
+    $rules = $acl.Access | ? {$_.IdentityReference -match ".*txcosc\\[^(s|d)]" -or $_.IdentityReference -Match ".*Domain Users.*"}
+    foreach ($rule in $rules) { 
+        $acl.RemoveAccessRule($rule);
+	}
 
     $name = ($fol.Name)
-    $NewAccessRule = New-Object system.security.AccessControl.FileSystemAccessRule("txcosc\$name","Read",,,"Allow")
+    $NewAccessRule = New-Object system.security.AccessControl.FileSystemAccessRule("txcosc\$name","Modify",,,"Allow")
     $acl.AddAccessRule($NewAccessRule)
     Set-Acl -path $fol -AclObject $acl
+}
 }
