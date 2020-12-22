110811d35a65009cb305c4882716b1d50d0a8ac5
JacobPackman <jacobvdi@VDI-ITS04.localdomain>
2020-12-22 08:23:17 -0600

    Redid Inherit permissions to allow propagation to children

--- RedoAcl.ps1
+++ RedoAcl.ps1
@@ -5,13 +5,13 @@ foreach ($fol in $folders) {
     $acl.SetAccessRuleProtection($true,$true)
     Set-Acl -path $fol -AclObject $acl
     
-    $rules = $acl.Access | ? {$_.IdentityReference -match ".*txcosc\\[^(s|d)]" -or $_.IdentityReference -Match ".*Domain Users.*"}
+    $rules = $acl.Access | ? {$_.IdentityReference -match ".*txcosc\\[^(s|d)]" -or $_.IdentityReference -Match ".*users.*"}
     foreach ($rule in $rules) { 
         $acl.RemoveAccessRule($rule);
 	}
 
     $name = ($fol.Name)
-    $NewAccessRule = New-Object system.security.AccessControl.FileSystemAccessRule("txcosc\$name","Modify",,,"Allow")
+    $NewAccessRule = New-Object system.security.AccessControl.FileSystemAccessRule("txcosc\$name","Modify","ObjectIherit, ContainerInherit","None","Allow")
     $acl.AddAccessRule($NewAccessRule)
     Set-Acl -path $fol -AclObject $acl
 }
