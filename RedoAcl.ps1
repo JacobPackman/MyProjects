6dd0bb80d30bf54e986b6a832d95942e6a14aca6
JacobPackman <jacobvdi@VDI-ITS04.localdomain>
2020-12-22 08:26:40 -0600

    Merged changes

--- RedoAcl.ps1
+++ RedoAcl.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $folders = gci -directory
 foreach ($fol in $folders) { 
    
@@ -17,22 +16,3 @@ foreach ($fol in $folders) {
     Set-Acl -path $fol -AclObject $acl
 }
 }
-=======
-$folders = gci -directory
-foreach ($fol in $folders) { 
-   
-    $acl = Get-Acl $fol
-    $acl.SetAccessRuleProtection($true,$true)
-    Set-Acl -path $fol -AclObject $acl
-    
-    $rules = $acl.Access | ? {$_.IdentityReference -match ".*txcosc\\[^(s|d)]" -or $_.IdentityReference -Match ".*Domain Users.*"}
-    foreach ($rule in $rules) { 
-        $acl.RemoveAccessRule($rule);
-	}
-
-    $name = ($fol.Name)
-    $NewAccessRule = New-Object system.security.AccessControl.FileSystemAccessRule("txcosc\$name","Modify",,,"Allow")
-    $acl.AddAccessRule($NewAccessRule)
-    Set-Acl -path $fol -AclObject $acl
-}
->>>>>>> 8f45a565a5ad37dfb2783ffd7ea20fd1a979871b
