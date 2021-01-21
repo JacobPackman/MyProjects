b0073c9881e62639105bc09e04b16d2647a4689d
JacobPackman <jpackman@DESKTOP-5JFUCRI.localdomain>
2021-01-21 12:13:03 -0600

    fixed error

--- Block-Email.ps1
+++ Block-Email.ps1
@@ -1,5 +1,5 @@
 write-host "### CONNECT TO EXCHANGE FIRST! ###"
 $email = read-host "Enter email given by end user" 
-$id = (Get-HostedContenFilterPolicy | ? {$_.IsDefault -eq $TRUE}).Id
+$id = (Get-HostedContentFilterPolicy | ? {$_.IsDefault -eq $TRUE}).Id
 Set-HostedContentFilterPolicy -Identity $id -BlockedSenders @{add="$email"}
 Get-HostedContentFilterPolicy -Identity $id | Select -Expand BlockedSenders
